592095659aeeb42ab6d2d03c837ef52253657aa9
KhaleddAhmed <kh.ahmed.farouk@gmail.com>
2025-02-21 00:48:04 +0200

    01-Building User Entity

---
+++ AthliQ.Core/Entities/AthliQUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace AthliQ.Core.Entities
+{
+    public class AthliQUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+        public bool IsAccepted { get; set; } = false;
+        public bool IsDeleted { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime ModifiedAt { get; set; }
+    }
+}
--- AthliQ.User.API/Program.cs
+++ AthliQ.User.API/Program.cs
@@ -1,9 +1,13 @@
+using AthliQ.Core.Entities;
+using AthliQ.Repository.Data.Contexts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace AthliQ.User.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +18,34 @@ namespace AthliQ.User.API
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddDbContext<AthliQDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("AthliQConnection"));
+            });
+
+            builder
+                .Services.AddIdentity<AthliQUser, IdentityRole>()
+                .AddEntityFrameworkStores<AthliQDbContext>();
+
             var app = builder.Build();
+
+            using var scope = app.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+
+            var _dbContext = services.GetRequiredService<AthliQDbContext>();
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+            try
+            {
+                await _dbContext.Database.MigrateAsync();
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex, "An Error Occured During Apply The Migration");
+            }
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -26,7 +57,6 @@ namespace AthliQ.User.API
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
