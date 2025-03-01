ad395f0bae0cff7a31ea037be892f553878c73a4
KhaleddAhmed <kh.ahmed.farouk@gmail.com>
2025-03-02 01:39:58 +0200

    Account Module

---
+++ AthliQ.Core/DTOs/Auth/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AthliQ.Core.DTOs.Auth
+{
+    public class UserDto
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+
+        public string Token { get; set; }
+    }
+}
---
+++ AthliQ.Core/Mapping/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AthliQ.Core.DTOs.Auth;
+using AthliQ.Core.Entities;
+using AutoMapper;
+
+namespace AthliQ.Core.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RegisterDto, AthliQUser>();
+        }
+    }
+}
