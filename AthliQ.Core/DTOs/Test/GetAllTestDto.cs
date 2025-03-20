c40138014c3542a6830ce0a9b455a90950a6d926
mohamed tarek <mohamad1tarek1@gmail.com>
2025-03-20 10:26:07 +0200

    adding description field to the GetAllTestDto

--- AthliQ.Core/DTOs/Test/GetAllTestDto.cs
+++ AthliQ.Core/DTOs/Test/GetAllTestDto.cs
@@ -11,5 +11,7 @@ namespace AthliQ.Core.DTOs.Test
         public int Id { get; set; }
         public string Name { get; set; }
         public string ArabicName { get; set; }
+
+        public string Description { get; set; }
     }
 }
