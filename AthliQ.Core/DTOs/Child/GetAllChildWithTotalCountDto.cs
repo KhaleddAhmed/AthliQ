925a11e8cedb7ffcf3929010fa4e13c20ba68cc5
salma1hosam <salma.hossam2003@gmail.com>
2025-03-21 04:13:13 +0200

    Adding TotalCount in the ViewAllChildern V3

---
+++ AthliQ.Core/DTOs/Child/GetAllChildWithTotalCountDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AthliQ.Core.DTOs.Child
+{
+	public class GetAllChildWithTotalCountDto
+	{
+        public int TotalCount { get; set; }
+        public List<GetAllChildDto> Children { get; set; }
+    }
+}
