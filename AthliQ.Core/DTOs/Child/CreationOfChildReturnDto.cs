5b7c020abc2d44d9cb295b656511e586f4326dda
khaledahmed2 <khaled.ahmed@appenza-studio.com>
2025-03-22 02:41:28 +0200

    Child Id Return in Creation Child

---
+++ AthliQ.Core/DTOs/Child/CreationOfChildReturnDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AthliQ.Core.DTOs.Child
+{
+    public class CreationOfChildReturnDto
+    {
+        public int ChildId { get; set; }
+        public bool IsCreated { get; set; }
+    }
+}
