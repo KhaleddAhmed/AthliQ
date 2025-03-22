856c4694fc5ecbd2687246bc695d687d1b5dcc1f
salma1hosam <salma.hossam2003@gmail.com>
2025-03-22 04:08:13 +0200

    Admin service (Accept , Reject , and Delete User)

---
+++ AthliQ.User.API/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using AthliQ.Core.Service.Contract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AthliQ.User.API.Controllers
+{
+	public class AdminController : BaseApiController
+	{
+		private readonly IAdminService _adminService;
+
+		public AdminController(IAdminService adminService)
+        {
+			_adminService = adminService;
+		}
+
+		[HttpPut("AcceptUser")]
+		[Authorize(Roles = "Admin")]
+		public async Task<ActionResult> Accept(string userId)
+		{
+			var user = await _adminService.AcceptUserAsync(userId);
+			return Ok(user);
+		}
+
+		[HttpPut("RejectUser")]
+		[Authorize(Roles = "Admin")]
+		public async Task<ActionResult> Reject(string userId)
+		{
+			var user = await _adminService.RejectUserAsync(userId);
+			return Ok(user);
+		}
+
+
+		[HttpPut("DeleteUser")]
+		[Authorize(Roles = "Admin")]
+		public async Task<ActionResult> Delete(string userId)
+		{
+			var user = await _adminService.DeleteUserAsync(userId);
+			return Ok(user);
+		}
+	}
+}
